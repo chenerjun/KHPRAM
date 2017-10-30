9bae688f7c1986cbd7ed72b59d170fd0365daa78
William Chen <chenerjun@gmail.com>
2017-10-30 11:36:58 -0400

    Oct30,2017 added new SP to ETL Process

--- SQLScript/crd.V_CommunityResource.sql
+++ SQLScript/crd.V_CommunityResource.sql
@@ -1,7 +1,7 @@
 USE [RAM]
 GO
 
-/****** Object:  View [crd].[V_CommunityResource]    Script Date: 10/19/2017 9:33:07 PM ******/
+/****** Object:  View [crd].[V_CommunityResource]    Script Date: 10/25/2017 11:37:07 AM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -13,13 +13,13 @@ GO
  
  
 
-CREATE VIEW [crd].[V_CommunityResource]
+ALTER VIEW [crd].[V_CommunityResource]
 AS
 SELECT        v.ETLLoadID, v.ResourceAgencyNum, v.PublicName, v.TaxonomyLevelName, v.TaxonomyCodes, v.ConnectsToSiteNum
 , v.ConnectsToProgramNum, v.ParentAgencyNum, c.CityId, v.city, v.ProvinceID, v.palias, v.Latitude 
 ,v.Longitude, v.phone, v.LanguageOfRecord, v.DisabilitiesAccess, v.AgencyDescription
 , v.FeeStructureSource, v.CustomEligibilitybyAge, v.CoverageArea, v.Coverage, v.WebsiteAddress, v.WorkHours, v.HoursOfOperation  
-,v.Eligibility, v.ApplicationProcess, v.DocumentsRequired, v.NormalWaitTime, v.LanguagesOfferedList, v.IsLinkOnly
+,v.Eligibility, v.ApplicationProcess, v.DocumentsRequired, v.NormalWaitTime, v.LanguagesOfferedList, v.IsLinkOnly,v.CustomRecordType
 ,[EnteredOn]
 ,[UpdatedOn]
 FROM            [crd].[V_CommunityResourceRAW] AS v LEFT OUTER JOIN
