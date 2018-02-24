cbf28dd98e30e5ca2bf24aa380fe4e28307a3fb7
William Chen <chenerjun@gmail.com>
2018-02-23 23:43:49 -0500

    check

--- SQLScript/V_All_User_From_City.sql
+++ SQLScript/V_All_User_From_City.sql
@@ -1,15 +1,15 @@
-USE [RAM]
+﻿USE [RAM]
 GO
 
-/****** Object:  View [dbo].[V_LastMonth_User_From_City]    Script Date: 8/13/2017 7:43:23 PM ******/
+/****** Object:  View [dbo].[V_All_User_From_City]    Script Date: 2/22/2018 10:41:14 PM ******/
+/******  这个数据包含问卷调查的  **********/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-CREATE view [dbo].[V_All_User_From_City]
+ALTER view [dbo].[V_All_User_From_City]
  AS
  SELECT   a.*  from
 (select top (10) cscity + ', '+csregion+', '+ cscountry as [city] , count(apilogid) as [num]
@@ -18,7 +18,5 @@ where  cscountry ='CA' AND cscity != ''
 group by cscity + ', '+csregion+', '+ cscountry 
 order by num desc) as a 
 GO
-
-
 
 
